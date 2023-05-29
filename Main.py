c441d7bade93d07bc58702d0328f92e81998abdf
bhavaygoel <goelbhavay@gmail.com>
2023-05-29 18:27:31 +0530

    added winning logic

--- Main.py
+++ Main.py
@@ -14,6 +14,28 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+    return winnings, winning_lines
+
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
@@ -60,9 +82,15 @@ def get_number_of_lines():
             print("Please enter valid lines ")
     return lines
 
+
 def print_slot_machine(columns):
     for row in range(len(columns[0])):
-        
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row])
+
 
 def get_bet():
     while True:
@@ -92,5 +120,9 @@ def main():
     print(
         f"You are betting ${bet} on {lines} lines. Your total bet is: ${total_bet}")
 
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won {winning_lines} and ${winnings}")
 
 main()
